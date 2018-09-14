dab602b2b3a76b4ceaf641163a34c2af2a3db59f
Wendy <wryao64@gmail.com>
2018-09-14 13:11:45 +1200

    Displays title on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
 import * as React from 'react';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import createStyles from '@material-ui/core/styles/createStyles';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import withRoot from './withRoot';
+// import { ButtonToolbar } from 'react-bootstrap';
 
-export default class App extends React.Component<{}> {
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      textAlign: 'center',
+      paddingTop: theme.spacing.unit * 20,
+    },
+  });
+
+const API = 'https://openlibrary.org/subjects/';
+const ending = '.json';
+
+class App extends React.Component<WithStyles<typeof styles>> {
+  constructor(props:any) {
+    super(props);
+  }
+
+  state = {
+    genre: '',
+    hits: [],
+  }
+
+  getBookData(genre: String) {
+    fetch(API + genre + ending)
+    .then(response => response.json())
+    .then(data => this.setState({ hits: data.hits }));
+  }
+
+  handleChange = (genre:any) => (event:any) => {
+    this.setState({
+      [genre]: event.target.value,
+    });
+  };
+
   public render() {
     return (
       <div className="container-fluid">
@@ -12,17 +49,19 @@ export default class App extends React.Component<{}> {
           {/* React components must have a wrapper node/element */}
           <h1>BookIt</h1>
           <h2>Run away with a new book in your hand!</h2>
+
           <div className="search">
-            <Input
-              placeholder="Genre"
-              // className={classes.input}
-              inputProps={{
-                'aria-label': 'Description',
-              }}
+            <TextField
+              id="genre"
+              label="Genre"
+              className="textField"
+              value={this.state.genre}
+              onChange={this.handleChange('genre')}
+              margin="normal"
             />
-            <p></p>
+
             <Link to="/FirstComponent">
-              <Button>CLICK ME</Button>
+              <Button variant="outlined" className="btn" onClick={() => this.getBookData(this.state.genre)}>SEARCH</Button>
             </Link>
           </div>
         </div>
@@ -30,3 +69,5 @@ export default class App extends React.Component<{}> {
     );
   }
 }
+
+export default withRoot(withStyles(styles)(App));
--- src/components/FirstComponent.tsx
+++ src/components/FirstComponent.tsx
@@ -1,12 +1,39 @@
 import * as React from "react";
 
-export default class FirstComponent extends React.Component<{}> {
+const API = 'https://openlibrary.org/subjects/';
+const ending = '.json';
+
+export default class FirstComponent extends React.Component<{}, {bookData: [], requestFailed: boolean}> {
+        constructor(props:any) {
+                super(props);
+                this.state = {
+                        bookData: [],
+                        requestFailed: false,
+                }
+        }
+
+        componentDidMount() {
+                fetch(API + "love" + ending)
+                .then(response => response.json())
+                .then(json => {
+                        this.setState({
+                                bookData: json.works[0].title
+                        })
+                }, () => {
+                        this.setState({
+                                requestFailed: true
+                        })
+                })
+        }
 
         public render() {
+                if (this.state.requestFailed) return <p>Failed</p>
+                if (this.state.bookData === []) return <p>Loading...</p>
                 return (
                         <div className="centreText">
                                 {/* React components must have a wrapper node/element */}
-                                <h1>(▀̿Ĺ̯▀̿ ̿)</h1>
+                                {/* <h1>And the book is...</h1> */}
+                                <p>{this.state.bookData}</p>
                         </div>
                 );
         }
